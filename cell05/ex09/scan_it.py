1ad2e2347324b928fe71bec1746e1c5d05e53c74
unknown <66050295@kmitl.ac.th>
2025-09-17 23:57:53 +0700

    Initial commit

--- cell05/ex09/scan_it.py
+++ cell05/ex09/scan_it.py
@@ -1,19 +1,22 @@
+#!/usr/bin/env python3
 import sys
 import re
 
 args = sys.argv[1:]
+
 if len(args) != 2:
     print("none")
 else:
     keyword = args[0]
     text = args[1]
 
-    pattern = f'(?={keyword})'
-    matches = re.findall(pattern, text)
-    if not matches:
+    matches = re.findall(re.escape(keyword), text)
+    if len(matches) == 0:
         print("none")
     else:
         print(len(matches))
+
+
 
 
 
